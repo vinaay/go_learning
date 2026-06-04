690ff1955361656edd4c62d840886f54dd0057dd
vinay <vinay.mbkumar@gmail.com>
2026-06-04 20:51:35 +0530

    commented the error

--- concepts/error/main.go
+++ concepts/error/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 )
 
 func divide(a, b float64) (float64, error) {
@@ -12,11 +11,11 @@ func divide(a, b float64) (float64, error) {
 	return a / b, nil
 }
 
-func main() {
-	result, err := divide(10, 0)
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Result:", result)
-	}
-}
+// func main() {
+// 	result, err := divide(10, 0)
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 	} else {
+// 		fmt.Println("Result:", result)
+// 	}
+// }
